d4960f5c72728261df015827cca866667b637d52
wardcolm <wardcolm@gmail.com>
2024-12-05 22:33:13 +0000

    Update Storage.js

--- shop/Storage.js
+++ shop/Storage.js
@@ -1,3 +1,21 @@
+const productsPrice = new Map([
+  ["bedShop1", 10],
+  ["bedShop2", 20],
+  ["bedShop3", 10],
+  ["bedShop4", 30],
+  ["bedShop5", 40],
+  ["bedShop6", 45],
+  ["bedShop7", 14],
+  ["bedShop8", 16],
+  ["bedShop9", 10],
+  ["bedShop10", 8],
+  ["bedShop11", 6],
+  ["bedShop12", 89],
+  ["bedShop13", 110],
+  ["bedShop14", 90],
+  ["bedShop15", 22],
+  ["bedShop16", 5]
+]);
 function SaveItem(name) {
 	if(localStorage.getItem(name) == null){
 		localStorage.setItem(name, 1);
@@ -44,17 +62,16 @@ function ClearAll() {
 function doShowAll() {
 	if (CheckBrowser()) {
 		var key = "";
-		var list = "<tr><th>Item</th><th>Value</th></tr>\n";
+		var list = " <tr><th>Item</th><th>Price</th><th>Quantity</th><th>Total</th><th>Action</th></tr>\n";
 		var i = 0;
 		//for more advance feature, you can set cap on max items in the cart
 		for (i = 0; i <= localStorage.length-1; i++) {
 			key = localStorage.key(i);
-			list += "<tr><td>" + key + "</td>\n<td>"
-					+ localStorage.getItem(key) + "</td></tr>\n";
+			list += "<tr><td>" + key + "</td>\n<td>"+ "€"+productsPrice.get(key) +"</td>\n<td>"+ localStorage.getItem(key) +"</td>\n<td>"+ "€" + productsPrice.get(key)*localStorage.getItem(key)+ "</td></tr>\n";
 		}
 		//if no item exists in the cart
-		if (list == "<tr><th>Item</th><th>Value</th></tr>\n") {
-			list += "<tr><td><i>empty</i></td>\n<td><i>empty</i></td></tr>\n";
+		if (list == "<tr><th>Item</th><th>Price</th><th>Quantity</th><th>Total</th><th>Action</th></tr>\n") {
+			list += "<tr><td><i>empty</i></td>\n<td><i>empty</i></td>\n<td><i>empty</i></td><td><i>empty</i></td>\n<td><i>empty</i></td></tr>\n";
 		}
 		//bind the data to html table
 		//you can use jQuery too....
@@ -63,6 +80,7 @@ function doShowAll() {
 		alert('Cannot save shopping list as your browser does not support HTML 5');
 	}
 }
+
 
 /*
  =====> Checking the browser support
